f8da4aec4c189e2bedd7c8cee43f9ff169f465f0
Douglas Gomes <douglasmg7@gmail.com>
2016-08-21 12:44:01 -0300

    new register to include

--- pet.sql
+++ pet.sql
@@ -2,4 +2,5 @@ INSERT INTO pet VALUES ('Puffball','Diane','hamster','f','1999-03-30');
 INSERT INTO pet VALUES ('Koque','Márcia','cachorro','f','2006-04-28');
 INSERT INTO pet VALUES ('Tug','Paulo','gato','m','2016-01-25');
 INSERT INTO pet VALUES ('Pirata','Felipe','cachorro','m','2015-03-12');
+INSERT INTO pet VALUES ('Felix','Ricardo','cachorro','m','2016-08-15');
 
